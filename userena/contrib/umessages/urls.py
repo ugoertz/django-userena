a7da562df18dc0ad22425d516ed48c93c3211f05
Ulrich Goertz <ug@geometry.de>
2015-04-14 13:47:12 +0200

    Allow @ and - in usernames also in umessages.

--- userena/contrib/umessages/urls.py
+++ userena/contrib/umessages/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
         messages_views.message_compose,
         name='userena_umessages_compose'),
 
-    url(r'^compose/(?P<recipients>[\+\.\w]+)/$',
+    url(r'^compose/(?P<recipients>[\@\+\.\w-]+)/$',
         messages_views.message_compose,
         name='userena_umessages_compose_to'),
 
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
         messages_views.message_compose,
         name='userena_umessages_reply'),
 
-    url(r'^view/(?P<username>[\.\w]+)/$',
+    url(r'^view/(?P<username>[\@\.\w-]+)/$',
         login_required(messages_views.MessageDetailListView.as_view()),
         name='userena_umessages_detail'),
 
